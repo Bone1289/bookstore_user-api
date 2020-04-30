a3df62cf4c7239681aa9960c0baab497eb30dbc5
Rata Bogdan <bogdan.rata1289@gmail.com>
2020-04-30 15:04:54 +0300

    Adding create user rest end point.

--- controllers/user/user_controller.go
+++ controllers/user/user_controller.go
@@ -1,12 +1,10 @@
 package user
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/Bone1289/bookstore_user-api/domain/users"
 	"github.com/Bone1289/bookstore_user-api/service"
+	"github.com/Bone1289/bookstore_user-api/utils/errors"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -16,22 +14,30 @@ func GetUser(c *gin.Context) {
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	fmt.Println(user)
-	bytes, err := ioutil.ReadAll(c.Request.Body)
+	//fmt.Println(user)
+	//bytes, err := ioutil.ReadAll(c.Request.Body)
+	//
+	//if err != nil {
+	//	//TODO: Handle error
+	//	return
+	//}
+	//
+	//if err := json.Unmarshal(bytes, &user); err != nil {
+	//	fmt.Println(err.Error())
+	//	//TODO: Handle error
+	//	return
+	//}
 
-	if err != nil {
-		//TODO: Handle error
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+
 		return
 	}
 
-	if err := json.Unmarshal(bytes, &user); err != nil {
-		fmt.Println(err.Error())
-		//TODO: Handle error
-		return
-	}
 	result, saveError := service.CreateUser(user)
 	if saveError != nil {
-		//TODO: handle user creation error
+		c.JSON(saveError.Status, saveError)
 		return
 	}
 
--- service/users_service.go
+++ service/users_service.go
@@ -2,8 +2,9 @@ package service
 
 import (
 	"github.com/Bone1289/bookstore_user-api/domain/users"
+	"github.com/Bone1289/bookstore_user-api/utils/errors"
 )
 
-func CreateUser(user users.User) (*users.User, error) {
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	return &user, nil
 }
