054d978567a86ceb9720d1766e26639bb6e64ec9
Rata Bogdan <bogdan.rata1289@gmail.com>
2020-05-12 20:04:45 +0300

    Add correct sql for create user table.

--- resource/01_create_users.sql
+++ resource/01_create_users.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `user_db`.`new_table` (
+CREATE TABLE `user_db`.`users` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NULL,
   `last_name` VARCHAR(45) NULL,
   `email` VARCHAR(45) NOT NULL,
-  `date` VARCHAR(45) NULL,
+  `date_created` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
